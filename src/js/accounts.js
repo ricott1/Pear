63e5dd07e73c5614fcb087a30bc4d52d248e4003
ricott1 <ricott2@gmail.com>
2017-10-14 08:18:28 -0400

    index

---
+++ src/js/accounts.js
@@ -0,0 +1,5 @@
+var accountsData = {
+    "Pericles" : {"submissionKey" : "Idontknow", "address" : "0xa33234fabbbad66677"},
+	"Carlos" : {"submissionKey" : "munothiella", "address" : "0xa3323dafabbbad66677"},
+	"Ale" : {"submissionKey" : "ale", "address" : "0xa1323dafabbbad66677"}
+    };
