8e955f4becfe5e63861424436a8c0dc78584fc8f
ricott1 <ricott2@gmail.com>
2017-10-14 03:49:03 -0400

    use of require, changed reviews to be like papers

--- contracts/Agora.sol
+++ contracts/Agora.sol
@@ -18,8 +18,7 @@ contract Agora
     struct Paper {
       address author;
       uint field;
-      Review[3] reviews;
-      uint numReviews;
+      address[] reviewKeys;
       uint timestamp;
       bool exists;
     }
@@ -39,16 +38,16 @@ contract Agora
     }
  
     function newAccount() public returns(bool) {
-        //require(!hasAccount(msg.sender)); gonna use require after Metropolis
-        if (hasAccount(msg.sender)) return false;
+        require(!hasAccount(msg.sender)); //gonna use require after Metropolis
+        //if (hasAccount(msg.sender)) return false;
         accounts[msg.sender].hasAccount = true;
         accounts[msg.sender].reputation = [0,0,0];
         return true;
     }
 
     function deleteAccount(address addr) public onlyOwner returns(bool) {
-        //require(hasAccount(addr));
-        if (!hasAccount(addr)) return false;
+        require(hasAccount(addr));
+        //if (!hasAccount(addr)) return false;
         accounts[addr].hasAccount = false;
         return true;
     }
@@ -58,48 +57,55 @@ contract Agora
     }
 
     function submitPaper(uint field, uint stake, address key) public returns(bool) {
-        if (!hasAccount(msg.sender)) return false;
-        if (accounts[msg.sender].reputation[field] < stake) return false;
+        //if (!hasAccount(msg.sender)) return false;
+        //if (accounts[msg.sender].reputation[field] < stake) return false;
+        require (hasAccount(msg.sender));
+        require (hasAccount(msg.sender));
+        
+        require (newPaper(msg.sender, field, key));
         accounts[msg.sender].reputation[field] -= stake;
         accounts[msg.sender].stake[field] += stake;
-        if (!newPaper(msg.sender, field, key)) return false;
         return true;
     }
 
     function newPaper(address addr, uint f, address k) private returns(bool) {
-        if (!hasAccount(addr)) return false;
+        //if (!hasAccount(addr)) return false;
+        require (hasAccount(addr));
         Paper paper;
         paper.author = addr;
         paper.field = f;
         paper.timestamp = now;
         paper.exists = true;
-        paper.numReviews = 0;
         papers[k] = paper;
         return true;
     }
 
     function submitReview(uint field, uint stake, address paperKey, address reviewKey, uint[3] score) public returns(bool) {
-        if (!papers[paperKey].exists) return false;
-        if (!hasAccount(msg.sender)) return false;
-        if (accounts[msg.sender].reputation[field] <= 0) return false;
-        if (accounts[msg.sender].reputation[field] < stake) return false;
+        //if (!papers[paperKey].exists) return false;
+        //if (!hasAccount(msg.sender)) return false;
+        //if (accounts[msg.sender].reputation[field] <= 0) return false;
+        //if (accounts[msg.sender].reputation[field] < stake) return false;
+        require (papers[paperKey].exists);
+        require (hasAccount(msg.sender));
+        require (accounts[msg.sender].reputation[field] > 0);
+        require (accounts[msg.sender].reputation[field] >= stake);
+        
+        require (newReview(msg.sender, paperKey, reviewKey, score));
         accounts[msg.sender].reputation[field] -= stake;
         accounts[msg.sender].stake[field] += stake;
-        if (!newReview(msg.sender, paperKey, reviewKey, score)) return false;
           
         return true;
     }
 
     function newReview(address reviewerAddr, address paperKey, address k, uint[3] score) private returns(bool) {
-        if (!hasAccount(reviewerAddr)) return false;
+        //if (!hasAccount(reviewerAddr)) return false;
+        require (hasAccount(reviewerAddr));
         Review memory review;
         review.author = reviewerAddr;
         review.score = score;
         review.timestamp = now;
         review.exists = true;
-        if (papers[paperKey].numReviews >= 3) return false;
-        papers[paperKey].reviews[papers[paperKey].numReviews] = review; 
-        papers[paperKey].numReviews += 1;
+        papers[paperKey].reviewKeys.push(k);
         reviews[k] = review;
         return true;
     }
@@ -110,23 +116,26 @@ contract Agora
         //if (!papers[paperKey].exists) return invalid;
         //if (papers[paperKey].numReviews <= 0) return invalid;
         require(papers[paperKey].exists);
-        require(papers[paperKey].numReviews > 0);
         uint[3] memory score;
-        for (uint i = 0; i < papers[paperKey].numReviews; i++) {
+        uint N = papers[paperKey].reviewKeys.length;
+        for (uint i = 0; i < N; i++) {
+            address revKey = papers[paperKey].reviewKeys[i];
             for (uint j = 0; j < 3; j++) {
-                score[j] += papers[paperKey].reviews[i].score[j]/papers[paperKey].numReviews;
+                score[j] += reviews[revKey].score[j]/N;
             }
         }
         return score;
     }
 
     function getReputation(address addr, uint field) public constant returns(uint) {
-        if (!hasAccount(addr)) return 0;
+        //if (!hasAccount(addr)) return 0;
+        require (hasAccount(addr));
         return accounts[addr].reputation[field];
     }
 
     function getPaperAuthor(address key) private constant returns(address) {
         //if (!papers[key].exists) return 0;
+        require (papers[key].exists);
         return papers[key].author;
     }
 
