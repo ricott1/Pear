7ee7537d35a19e52abc7fd823de539142d5f5c1b
ricott1 <ricott2@gmail.com>
2017-10-15 05:54:43 -0400

    multi user

--- src/js/app.js
+++ src/js/app.js
@@ -23,14 +23,14 @@ App = {
 
   initContract: function() {
 
-    $.getJSON('Agora.json', function(data){
+    $.getJSON('Pear.json', function(data){
 
       // get contract artifacts
-      var AgoraArtifact = data;
-      App.contracts.Agora= TruffleContract(AgoraArtifact);  
+      var PearArtifact = data;
+      App.contracts.Pear= TruffleContract(PearArtifact);  
 
       //set provider
-      App.contracts.Agora.setProvider(App.web3Provider);  
+      App.contracts.Pear.setProvider(App.web3Provider);  
     })
 
 
@@ -54,15 +54,15 @@ App = {
   },
 
   createAccount : function(user) {
-    App.contracts.Agora.deployed().then(function(instance){
-      AgoraInstance = instance;
+    App.contracts.Pear.deployed().then(function(instance){
+      PearInstance = instance;
       
       //first test if the call is succesful
-      return AgoraInstance.newAccount.call();
+      return PearInstance.newAccount.call();
       
     }).then(function (value){
       //perform the real transaction
-      return AgoraInstance.newAccount.sendTransaction({from:web3.eth.coinbase, gas: 980000});
+      return PearInstance.newAccount.sendTransaction({from:web3.eth.coinbase, gas: 980000});
 
     }).then(function (v){
       localStorage.setItem(web3.eth.coinbase, user);
@@ -80,18 +80,18 @@ App = {
 
     paperKey = SHA1(text);
     console.log(paperKey);
-    App.contracts.Agora.deployed().then(function(instance){
-  		AgoraInstance = instance;
+    App.contracts.Pear.deployed().then(function(instance){
+  		PearInstance = instance;
   		stake = $("#stake").val();
 
 
     console.log(stake);
-  		return AgoraInstance.submitPaper.call(0, stake, paperKey);
+  		return PearInstance.submitPaper.call(0, stake, paperKey);
       
     }).then(function (value){
       //perform the real transaction
       console.log(stake, value);
-      return AgoraInstance.submitPaper.sendTransaction(0, stake, paperKey,{from:web3.eth.coinbase, gas: 180000});
+      return PearInstance.submitPaper.sendTransaction(0, stake, paperKey,{from:web3.eth.coinbase, gas: 180000});
 
     }).then(function (v){
       localStorage.setItem(paperKey, "paper");
@@ -99,18 +99,18 @@ App = {
   },
 
   reviewPaper: function(){
-    App.contracts.Agora.deployed().then(function(instance){
-  		AgoraInstace = instance;
+    App.contracts.Pear.deployed().then(function(instance){
+  		PearInstace = instance;
   		stake = 2;
   		score = [$("#reviewQuality").val(), $("#reviewImpact").val(), $("#reviewNovelty").val()] ;
       paperKey = $("#reviewInput").val();
       reviewKey = SHA1(paperKey + "ale");
-  		return AgoraInstance.submitReview.call(0, stake, paperKey, reviewKey, score);
+  		return PearInstance.submitReview.call(0, stake, paperKey, reviewKey, score);
       
       
     }).then(function (value){
       //perform the real transaction
-      return AgoraInstance.submitReview.sendTransaction(0, stake, paperKey, reviewKey, score,{from:web3.eth.coinbase, gas: 300000});
+      return PearInstance.submitReview.sendTransaction(0, stake, paperKey, reviewKey, score,{from:web3.eth.coinbase, gas: 300000});
 
     }).then(function (v){
       localStorage.setItem(reviewKey, "review");
@@ -133,11 +133,13 @@ App = {
           if (j >= paperstable.rows.length) {
             var row = paperstable.insertRow(j);
             var paperKeyCell = row.insertCell(0);
-            var paperTime = row.insertCell(1);
+            var paperStake = row.insertCell(1);
+            var paperTime = row.insertCell(2);
           } else{
             var row = paperstable.rows[j];
             var paperKeyCell = row.cells[0];
-            var paperTime = row.cells[1];
+            var paperStake = row.cells[1];
+            var paperTime = row.cells[2];
           }
           var btn = document.createElement('input');
           btn.type = "button";
@@ -155,6 +157,7 @@ App = {
           paperKeyCell.appendChild(btn);
 
           j ++;
+          App.getPaperStake(key, paperStake);
           App.getPaperTimestamp(key, paperTime);
         } else if(localStorage.getItem(key) != "review"){
           //console.log(localStorage.getItem(key), key);
@@ -183,9 +186,9 @@ App = {
   },
 
   getUserRep: function(key, field, entry){
-    App.contracts.Agora.deployed().then(function(instance){
-      AgoraInstance = instance;
-      return AgoraInstance.getReputation.call(key, field);
+    App.contracts.Pear.deployed().then(function(instance){
+      PearInstance = instance;
+      return PearInstance.getReputation.call(key, field);
           }).then(function(rep) {
             //console.log(rep.c[0], key);
             entry.innerHTML = rep.c[0];
@@ -194,11 +197,22 @@ App = {
   },
 
   getPaperTimestamp: function(key, entry){
-    App.contracts.Agora.deployed().then(function(instance){
-      AgoraInstance = instance;
-      return AgoraInstance.getPaperTimestamp.call(key);
+    App.contracts.Pear.deployed().then(function(instance){
+      PearInstance = instance;
+      return PearInstance.getPaperTimestamp.call(key);
           }).then(function(timestamp) {
             entry.innerHTML = timestamp;
+
+          })
+
+  },
+
+  getPaperStake: function(key, entry){
+    App.contracts.Pear.deployed().then(function(instance){
+      PearInstance = instance;
+      return PearInstance.getPaperStake.call(key);
+          }).then(function(stake) {
+            entry.innerHTML = stake;
 
           })
 
